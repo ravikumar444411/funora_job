2f0ca3e1fb0ab9edd285532a37c931713cf227fb
Ravi <ravikumar444411@gmail.com>
2026-03-10 12:23:14 +0530

    add cookies

--- src/automation_insta/controller/downloadVideo.js
+++ src/automation_insta/controller/downloadVideo.js
@@ -1,12 +1,26 @@
 const youtubedl = require("yt-dlp-exec");
 const fs = require("fs-extra");
+const path = require("path");
 
 async function downloadVideo(url, output = "input/video.mp4") {
+
   await fs.ensureDir("input");
 
-  await youtubedl(url, {
+  // remove tracking params like ?si=
+  const cleanUrl = url.split("?")[0];
+
+  const cookiesPath = path.join(
+    "/home/ec2-user/funora_job",
+    "www.youtube.com_cookies.txt"
+  );
+
+  await youtubedl(cleanUrl, {
     output: output,
-    format: "mp4"
+    format: "bv*+ba/b",
+    cookies: cookiesPath,
+    "sleep-interval": 2,
+    "max-sleep-interval": 5,
+    "js-runtimes": "node"
   });
 
   return output;
